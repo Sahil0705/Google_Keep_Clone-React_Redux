0e4c640719f1e2ac303cd6a949c6e212ec859eb1
Sahil <sahildonde7@gmail.com>
2022-01-26 19:49:47 +0530

    Handling wrong routing with react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ToDo from './components/todolist'
+import { Route, Redirect, Switch, BrowserRouter } from 'react-router-dom';
 
 const year = new Date().getFullYear();
 
 function App() {
   return(
     <>
-      <ToDo />
+      <BrowserRouter>
+       <Switch>
+         <Route exact path="/" component={ToDo} />
+         <Redirect to="/" />
+       </Switch>
+      </BrowserRouter>
       <div className='footer'>
       <p id='foot'>&copy; {year}, &nbsp;&nbsp;&nbsp; Made by - Sahil Sachin Donde &nbsp;😀</p>
       </div>

4ab3f15f49db0dc2e17dc9ba2d1fff9c78cb061b
Sahil <sahildonde7@gmail.com>
2022-01-26 20:25:51 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ToDo from './components/todolist'
-import { Route, Redirect, Switch, BrowserRouter } from 'react-router-dom';
 
 const year = new Date().getFullYear();
 
 function App() {
   return(
     <>
-      <BrowserRouter>
-       <Switch>
-         <Route exact path="/Google_Keep_Clone-React_Redux" component={ToDo} />
-         <Redirect to="/Google_Keep_Clone-React_Redux" />
-       </Switch>
-      </BrowserRouter>
+      <ToDo />
       <div className='footer'>
       <p id='foot'>&copy; {year}, &nbsp;&nbsp;&nbsp; Made by - Sahil Sachin Donde &nbsp;😀</p>
       </div>

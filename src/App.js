a947715804f95a4cdfcbd8f211bf8faa92265fb0
Sahil <sahildonde7@gmail.com>
2022-01-26 15:41:41 +0530

    Adding footer for gkeep clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ToDo from './components/todolist'
 
+const year = new Date().getFullYear();
+
 function App() {
   return(
     <>
       <ToDo />
+      <div class='footer'>
+      <p id='foot'>&copy; Copyright {year}, &nbsp;&nbsp;&nbsp; Made by - Sahil Sachin Donde😀</p>
+      </div>
     </>
   );
 }
